651ee3857eb83faa794cf9aea6297a261d87b861
Aero182 <dlewis151515@hotmail.com>
2025-10-20 18:22:58 +1100

    Point necromancer assets to existing vanilla textures

--- src/main/java/necromancermod/NecromancerMod.java
+++ src/main/java/necromancermod/NecromancerMod.java
@@ -1,13 +1,10 @@
 package necromancermod;
 
-import necromancermod.items.NecromancerWandItem;
+import necromancermod.items.NecromancerBoneWandItem;
 import necromancermod.mobs.NecromancerSummonedZombie;
-import necromancermod.mobs.SummonedZombieMob;
-import necromancermod.projectiles.NecroticBoltProjectile;
 import necesse.engine.modLoader.annotations.ModEntry;
 import necesse.engine.registries.ItemRegistry;
 import necesse.engine.registries.MobRegistry;
-import necesse.engine.registries.ProjectileRegistry;
 import necesse.engine.registries.Recipes;
 import necesse.inventory.recipe.Recipe;
 import necesse.inventory.recipe.ingredient.ItemIngredient;
@@ -18,15 +15,12 @@ import necesse.gfx.gameTexture.GameTexture;
 public class NecromancerMod {
 
     public void init() {
-        ItemRegistry.registerItem("necromancerwand", new NecromancerWandItem(), 30, true);
-        MobRegistry.registerMob("summonedzombie", SummonedZombieMob.class, true);
+        ItemRegistry.registerItem("necromancerwand", new NecromancerBoneWandItem(), 30, true);
         MobRegistry.registerMob("necromancer_summoned_zombie", NecromancerSummonedZombie.class, true);
-        ProjectileRegistry.registerProjectile("necroticbolt", NecroticBoltProjectile.class, "necroticbolt", "necroticbolt_shadow");
     }
 
     public void initResources() {
-        SummonedZombieMob.texture = GameTexture.fromFile("mobs/summonedzombie");
-        NecromancerSummonedZombie.texture = GameTexture.fromFile("mobs/summonedzombie");
+        NecromancerSummonedZombie.texture = GameTexture.fromFile("mobs/zombie");
     }
 
     public void postInit() {
--- src/main/java/necromancermod/items/NecromancerBoneWandItem.java
+++ src/main/java/necromancermod/items/NecromancerBoneWandItem.java
@@ -1,11 +1,15 @@
 package necromancermod.items;
 
 import necromancermod.mobs.NecromancerSummonedZombie;
+import necesse.engine.localization.Localization;
 import necesse.engine.network.gameNetworkData.GNDItemMap;
+import necesse.engine.util.GameBlackboard;
 import necesse.engine.util.GameRandom;
+import necesse.entity.mobs.PlayerMob;
 import necesse.entity.mobs.itemAttacker.ItemAttackerMob;
 import necesse.entity.mobs.summon.SummonedMob;
 import necesse.gfx.gameTexture.GameTexture;
+import necesse.gfx.gameTooltips.ListGameTooltips;
 import necesse.inventory.InventoryItem;
 import necesse.inventory.item.toolItem.ManaCost;
 import necesse.inventory.item.toolItem.summonToolItem.SummonToolItem;
@@ -31,6 +35,13 @@ public class NecromancerBoneWandItem extends SummonToolItem {
     public void setupAttack(Level level, int x, int y, ItemAttackerMob attackerMob, int attackHeight, InventoryItem item, int animAttack, int seed, GNDItemMap mapContent) {
         super.setupAttack(level, x, y, attackerMob, attackHeight, item, animAttack, seed, mapContent);
         consumeMana(attackerMob, item);
+    }
+
+    @Override
+    public ListGameTooltips getPreEnchantmentTooltips(InventoryItem item, PlayerMob perspective, GameBlackboard blackboard) {
+        ListGameTooltips tooltips = super.getPreEnchantmentTooltips(item, perspective, blackboard);
+        tooltips.add(Localization.translate("itemtooltip", "necromancerwandtip"));
+        return tooltips;
     }
 
     @Override
